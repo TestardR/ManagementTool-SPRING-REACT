2f3fbfd1429ebc8c4fac502f37430c7c19bc7277
Romain <rmtestard@gmail.com>
2018-10-28 18:50:34 +0100

    project object validation part1

--- src/main/java/io/romain/ppmtool/domain/Project.java
+++ src/main/java/io/romain/ppmtool/domain/Project.java
@@ -13,19 +13,18 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotBlank(message= "Project name is required")
+    @NotBlank(message = "Project name is required")
     private String projectName;
-    @NotBlank(message= "Project Identifier name is required")
+    @NotBlank(message ="Project Identifier is required")
     @Size(min=4, max=5, message = "Please use 4 to 5 characters")
     @Column(updatable = false, unique = true)
     private String projectIdentifier;
-    @NotBlank(message= "Description Identifier name is required")
+    @NotBlank(message = "Project description is required")
     private String description;
     @JsonFormat(pattern = "yyyy-mm-dd")
     private Date start_date;
     @JsonFormat(pattern = "yyyy-mm-dd")
     private Date end_date;
-
     @JsonFormat(pattern = "yyyy-mm-dd")
     private Date created_At;
     @JsonFormat(pattern = "yyyy-mm-dd")
@@ -104,7 +103,8 @@ public class Project {
     }
 
     @PreUpdate
-    protected void onUpdate() {
+    protected void onUpdate(){
         this.updated_At = new Date();
     }
+
 }
