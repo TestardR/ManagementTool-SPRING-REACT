46859533afd6de82fc2a764dbf55b618c408221c
Romain <rmtestard@gmail.com>
2018-10-29 14:27:38 +0100

    delete projects

--- ppmtool-react-client/src/actions/projectActions.js
+++ ppmtool-react-client/src/actions/projectActions.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
-import { GET_ERRORS, GET_PROJECTS, GET_PROJECT } from './types';
+import { GET_ERRORS, GET_PROJECTS, GET_PROJECT, DELETE_PROJECT } from './types';
 
 export const createProject = (project, history) => async dispatch => {
   try {
-    const res = await axios.post('/api/project', project);
+    const res = await axios.post('http://localhost:8080/api/project', project);
     history.push('/dashboard');
     dispatch({
       type: GET_ERRORS,
@@ -35,4 +35,12 @@ export const getProject = (id, history) => async dispatch => {
   } catch (error) {
     history.push('/dashboard');
   }
+};
+
+export const deleteProject = id => async dispatch => {
+  await axios.delete(`http://localhost:8080/api/project/${id}`);
+  dispatch({
+    type: DELETE_PROJECT,
+    payload: id
+  });
 };
--- ppmtool-react-client/src/components/Project/ProjectItem.js
+++ ppmtool-react-client/src/components/Project/ProjectItem.js
@@ -2,8 +2,13 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { deleteProject } from '../../actions/projectActions';
 
 class ProjectItem extends Component {
+  onDeleteClick = id => {
+    this.props.deleteProject(id);
+  };
+
   render() {
     const { project } = this.props;
     return (
@@ -19,18 +24,24 @@ class ProjectItem extends Component {
             </div>
             <div className="col-md-4 d-none d-lg-block">
               <ul className="list-group">
-                <Link to="">
+                <a href="#">
                   <li className="list-group-item board">
                     <i className="fa fa-flag-checkered pr-1"> Project Board </i>
                   </li>
-                </Link>
+                </a>
                 <Link to={`/updateProject/${project.projectIdentifier}`}>
                   <li className="list-group-item update">
                     <i className="fa fa-edit pr-1"> Update Project Info</i>
                   </li>
                 </Link>
 
-                <li className="list-group-item delete">
+                <li
+                  className="list-group-item delete"
+                  onClick={this.onDeleteClick.bind(
+                    this,
+                    project.projectIdentifier
+                  )}
+                >
                   <i className="fa fa-minus-circle pr-1"> Delete Project</i>
                 </li>
               </ul>
@@ -42,4 +53,11 @@ class ProjectItem extends Component {
   }
 }
 
-export default ProjectItem;
+ProjectItem.propTypes = {
+  deleteProject: PropTypes.func.isRequired
+};
+
+export default connect(
+  null,
+  { deleteProject }
+)(ProjectItem);
