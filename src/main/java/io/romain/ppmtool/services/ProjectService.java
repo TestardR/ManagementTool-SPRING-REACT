068327bc2a06f5daa545b3c7a2cb7fb46342ae86
Romain <rmtestard@gmail.com>
2018-10-29 07:54:32 +0100

    custom exceptions for Unique Project Identifiers

--- src/main/java/io/romain/ppmtool/services/ProjectService.java
+++ src/main/java/io/romain/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package io.romain.ppmtool.services;
 
 import io.romain.ppmtool.domain.Project;
+import io.romain.ppmtool.exceptions.ProjectIdException;
 import io.romain.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,9 +14,13 @@ public class ProjectService {
 
     public Project saveOrUpdateProject(Project project){
 
-        //Logic
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e){
+            throw new ProjectIdException("Project ID '"+ project.getProjectIdentifier().toUpperCase() + "' already exists");
+        }
 
-        return projectRepository.save(project);
     }
 
 }
