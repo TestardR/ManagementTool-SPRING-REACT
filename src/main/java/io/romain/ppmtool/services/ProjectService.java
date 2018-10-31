d1b5e9ff06c81df31e1810b591a756a402004c69
Romain <rmtestard@gmail.com>
2018-10-31 15:58:04 +0100

    User: OneToMany Projects

--- src/main/java/io/romain/ppmtool/services/ProjectService.java
+++ src/main/java/io/romain/ppmtool/services/ProjectService.java
@@ -2,9 +2,11 @@ package io.romain.ppmtool.services;
 
 import io.romain.ppmtool.domain.Backlog;
 import io.romain.ppmtool.domain.Project;
+import io.romain.ppmtool.domain.User;
 import io.romain.ppmtool.exceptions.ProjectIdException;
 import io.romain.ppmtool.repositories.BacklogRepository;
 import io.romain.ppmtool.repositories.ProjectRepository;
+import io.romain.ppmtool.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,10 +19,17 @@ public class ProjectService {
     @Autowired
     private BacklogRepository backlogRepository;
 
+    @Autowired
+    private UserRepository userRepository;
 
-    public Project saveOrUpdateProject(Project project){
+
+    public Project saveOrUpdateProject(Project project, String username){
 
         try {
+
+            User user = userRepository.findByUsername(username);
+            project.setUser(user);
+            project.setProjectLeader(user.getUsername());
             project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
 
             if(project.getId()==null){
